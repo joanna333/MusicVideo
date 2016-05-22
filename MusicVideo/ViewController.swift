0cf1049c83db888fe4590b91396e87987ecb224a
Joanna Kolny-Olesiak <joanna@kolny.de>
2016-05-22 18:12:59 +0200

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Joanna Kolny-Olesiak on 22.05.16.
+//  Copyright © 2016 Joanna Kolny-Olesiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
