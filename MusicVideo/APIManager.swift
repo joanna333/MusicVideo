ad1a4392db52fc0e40953ec0ec4e9ca72a7819c9
Joanna Kolny-Olesiak <joanna@kolny.de>
2016-05-22 20:13:07 +0200

    Completed Part 2 Coding Session

---
+++ MusicVideo/APIManager.swift
@@ -0,0 +1,36 @@
+//
+//  APIManager.swift
+//  MusicVideo
+//
+//  Created by Joanna Kolny-Olesiak on 22.05.16.
+//  Copyright © 2016 Joanna Kolny-Olesiak. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    func loadData(urlString: String, completion: (result:String) -> Void) {
+        
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        
+ //       let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        let task = session.dataTaskWithURL(url) {
+            (data, response, error) -> Void in
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                if error != nil {
+                    completion(result: (error!.localizedDescription))
+                } else {
+                    completion(result: "NSURLSession successful")
+                    print(data)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
