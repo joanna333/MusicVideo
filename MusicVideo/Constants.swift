12f701ed3b2bd1a2e680b929407e4f45f05e3c23
Joanna Kolny-Olesiak <joanna@kolny.de>
2016-05-23 09:26:20 +0200

    Part 4 Completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Joanna Kolny-Olesiak on 23.05.16.
+//  Copyright © 2016 Joanna Kolny-Olesiak. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
